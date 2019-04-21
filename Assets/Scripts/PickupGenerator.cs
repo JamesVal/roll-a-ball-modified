7aa2de7dc9f05fb6d504e2e9c05c6d5a500709e7
JamesVal <james.jo.val@gmail.com>
2019-04-20 23:05:09 -0700

    first commit

---
+++ Assets/Scripts/PickupGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupGenerator : MonoBehaviour
+{
+    public GameObject gObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.createNewGObj();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void createNewGObj()
+    {
+        Vector3 randomCoords = this.getRandomCoordinates();
+        Instantiate(gObj, randomCoords, Quaternion.identity);
+    }
+
+    Vector3 getRandomCoordinates()
+    {
+        System.Random rd = new System.Random();
+
+        int x = rd.Next(-40,40);
+        int z = rd.Next(-40,40);
+        
+        Vector3 newCoord = new Vector3(x, 2.5f, z);
+
+        return newCoord;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private int counter;
+    public Text currentText;
+    public Text winText;
+    private Rigidbody rb;
+    public PickupGenerator pickupGenerator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        counter = 0;
+        this.updateText();
+        winText.text = "";
+    }
+
+    void updateText()
+    {
+        currentText.text = "Count: " + counter.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 movement =  new Vector3(h, 0, v);
+
+        rb.AddForce(movement * speed);
+    }
+
+    // Destroy everything that enters the trigger
+    void OnTriggerEnter(Collider otherObject)
+    {
+        if (otherObject.gameObject.CompareTag("Pickup"))
+        {
+            otherObject.gameObject.SetActive(false);
+            this.pickupGenerator.createNewGObj();
+            counter++;
+            if (counter == 5) winText.text = "DUNZO?";
+            this.updateText();
+        }
+    }
+}
